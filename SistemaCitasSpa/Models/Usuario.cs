65f835bbe512f14da441da3f44e2f8d20ddd0420
emely1722 <66326292+emely1722@users.noreply.github.com>
2026-07-15 23:39:39 -0400

    modificación del modelo Usuario

--- SistemaCitasSpa/Models/Usuario.cs
+++ SistemaCitasSpa/Models/Usuario.cs
@@ -1,30 +1,29 @@
 ﻿using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 
 namespace SistemasCitasSpa.Models
 {
-    [Table("Usuarios")]
     public class Usuario
     {
-        [Key]
         public int IdUsuario { get; set; }
 
-        [Required]
-        [MaxLength(120)]
-        public string NombreCompleto { get; set; } = string.Empty;
+        [Required(ErrorMessage = "Nombre completo es requerido")]
+        [StringLength(120, MinimumLength = 3,
+            ErrorMessage = "El nombre debe tener de 3 a 120 caracteres")]
+        public required string NombreCompleto { get; set; }
 
-        [Required]
-        [MaxLength(50)]
-        public string NombreUsuario { get; set; } = string.Empty;
+        [Required(ErrorMessage = "Nombre de usuario es requerido")]
+        [StringLength(50, MinimumLength = 3,
+            ErrorMessage = "El usuario debe tener de 3 a 50 caracteres")]
+        public required string NombreUsuario { get; set; }
 
-        [Required]
-        [EmailAddress]
-        [MaxLength(120)]
-        public string Correo { get; set; } = string.Empty;
+        [Required(ErrorMessage = "Correo es requerido")]
+        [EmailAddress(ErrorMessage = "Formato de correo incorrecto")]
+        [StringLength(120, ErrorMessage = "El correo no puede exceder 120 caracteres")]
+        public required string Correo { get; set; }
 
-        [Required]
-        [MaxLength(255)]
-        public string ClaveHash { get; set; } = string.Empty;
+        [Required(ErrorMessage = "Clave requerida")]
+        [StringLength(255, ErrorMessage = "La clave no puede exceder 255 caracteres")]
+        public required string ClaveHash { get; set; }
 
         public bool Activo { get; set; } = true;
     }
