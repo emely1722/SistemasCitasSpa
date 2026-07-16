e815d13483d766adc3b7ba13cb81ddd759917278
emely1722 <66326292+emely1722@users.noreply.github.com>
2026-07-15 23:58:54 -0400

    Creación Sala.cs

---
+++ SistemaCitasSpa/Models/Sala.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SistemasCitasSpa.Models
+{
+    public class Sala
+    {
+        public int IdSala { get; set; }
+
+        [Required(ErrorMessage = "El nombre de la sala es requerido")]
+        [StringLength(80, MinimumLength = 3,
+            ErrorMessage = "El nombre debe tener de 3 a 80 caracteres")]
+        public required string Nombre { get; set; }
+
+        [StringLength(250,
+            ErrorMessage = "La descripción no puede exceder 250 caracteres")]
+        public string? Descripcion { get; set; }
+
+        public bool Activa { get; set; } = true;
+
+        public List<Cita> Citas { get; set; } = new();
+    }
+}
