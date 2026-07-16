6c16b769c15e29beb72d96550473430d52578c05
emely1722 <66326292+emely1722@users.noreply.github.com>
2026-07-15 23:57:52 -0400

    Creación HorarioTerapeuta.cs

---
+++ SistemaCitasSpa/Models/HorarioTerapeuta.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SistemasCitasSpa.Models
+{
+    public class HorarioTerapeuta
+    {
+        public int IdHorario { get; set; }
+
+        [Range(1, int.MaxValue, ErrorMessage = "Debe seleccionar un terapeuta")]
+        public int IdTerapeuta { get; set; }
+
+        [Required(ErrorMessage = "El día de la semana es requerido")]
+        [StringLength(15,
+            ErrorMessage = "El día no puede exceder 15 caracteres")]
+        public required string DiaSemana { get; set; }
+
+        public TimeOnly HoraInicio { get; set; }
+
+        public TimeOnly HoraFin { get; set; }
+
+        public bool Activo { get; set; } = true;
+
+        public Terapeuta? Terapeuta { get; set; }
+    }
+}
