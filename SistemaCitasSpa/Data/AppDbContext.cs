f4534e83181d2cc96044371bcfb4548eb948830f
emely1722 <66326292+emely1722@users.noreply.github.com>
2026-07-15 23:53:09 -0400

    Creación Cliente.cs

--- SistemaCitasSpa/Data/AppDbContext.cs
+++ SistemaCitasSpa/Data/AppDbContext.cs
@@ -1,6 +1,22 @@
-﻿namespace SistemaCitasSpa.Data
+﻿using Microsoft.EntityFrameworkCore;
+using SistemasCitasSpa.Models;
+
+namespace SistemasCitasSpa.Data
 {
-    public class AppDbContext
+    public class ApplicationDbContext : DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<CategoriaServicio> CategoriasServicios { get; set; }
+        public DbSet<Servicio> Servicios { get; set; }
+        public DbSet<Terapeuta> Terapeutas { get; set; }
+        public DbSet<HorarioTerapeuta> HorariosTerapeutas { get; set; }
+        public DbSet<Sala> Salas { get; set; }
+        public DbSet<MetodoPago> MetodosPago { get; set; }
+        public DbSet<Cita> Citas { get; set; }
     }
 }
---
+++ SistemaCitasSpa/Models/Cliente.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SistemasCitasSpa.Models
+{
+    public class Cliente
+    {
+        public int IdCliente { get; set; }
+
+        [Required(ErrorMessage = "El nombre es requerido")]
+        [StringLength(60, MinimumLength = 2,
+            ErrorMessage = "El nombre debe tener de 2 a 60 caracteres")]
+        public required string Nombre { get; set; }
+
+        [Required(ErrorMessage = "El apellido es requerido")]
+        [StringLength(60, MinimumLength = 2,
+            ErrorMessage = "El apellido debe tener de 2 a 60 caracteres")]
+        public required string Apellido { get; set; }
+
+        [Required(ErrorMessage = "El teléfono es requerido")]
+        [StringLength(20, ErrorMessage = "El teléfono no puede exceder 20 caracteres")]
+        public required string Telefono { get; set; }
+
+        [EmailAddress(ErrorMessage = "El correo no tiene un formato válido")]
+        [StringLength(120, ErrorMessage = "El correo no puede exceder 120 caracteres")]
+        public string? Correo { get; set; }
+
+        public bool Activo { get; set; } = true;
+
+        public List<Cita> Citas { get; set; } = new();
+    }
+}
