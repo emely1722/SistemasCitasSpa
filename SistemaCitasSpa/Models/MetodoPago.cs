4f1b39a607f69f001ef80ba4e3671ef64baf9dba
emely1722 <66326292+emely1722@users.noreply.github.com>
2026-07-15 23:59:47 -0400

    Creación MetodoPago.cs

---
+++ SistemaCitasSpa/Models/MetodoPago.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SistemasCitasSpa.Models
+{
+    public class MetodoPago
+    {
+        public int IdMetodoPago { get; set; }
+
+        [Required(ErrorMessage = "El nombre del método de pago es requerido")]
+        [StringLength(60, MinimumLength = 3,
+            ErrorMessage = "El nombre debe tener de 3 a 60 caracteres")]
+        public required string Nombre { get; set; }
+
+        [StringLength(200,
+            ErrorMessage = "La descripción no puede exceder 200 caracteres")]
+        public string? Descripcion { get; set; }
+
+        public bool Activo { get; set; } = true;
+
+        public List<Cita> Citas { get; set; } = new();
+    }
+}
