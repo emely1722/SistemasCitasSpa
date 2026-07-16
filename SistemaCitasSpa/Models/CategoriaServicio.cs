770f39d01f1b1262563db7ac5d6c2e615df5c2c0
emely1722 <66326292+emely1722@users.noreply.github.com>
2026-07-15 23:54:46 -0400

    Creación CategoriaServicio.cs

---
+++ SistemaCitasSpa/Models/CategoriaServicio.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SistemasCitasSpa.Models
+{
+    public class CategoriaServicio
+    {
+        public int IdCategoria { get; set; }
+
+        [Required(ErrorMessage = "El nombre de la categoría es requerido")]
+        [StringLength(80, MinimumLength = 3,
+            ErrorMessage = "El nombre debe tener de 3 a 80 caracteres")]
+        public required string Nombre { get; set; }
+
+        [StringLength(250,
+            ErrorMessage = "La descripción no puede exceder 250 caracteres")]
+        public string? Descripcion { get; set; }
+
+        public bool Activo { get; set; } = true;
+
+        public List<Servicio> Servicios { get; set; } = new();
+    }
+}
